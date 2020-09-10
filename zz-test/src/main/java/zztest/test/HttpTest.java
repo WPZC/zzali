704cb45a2a5b6580aaf992e417325653c2170c26
WPZC <1272722954@qq.com>
2020-09-10 16:46:50 +0800

    http回调不解

--- zz-test/src/main/java/zztest/test/HttpTest.java
+++ zz-test/src/main/java/zztest/test/HttpTest.java
@@ -1,9 +1,12 @@
 package zztest.test;
 
+import com.netflix.client.http.HttpRequest;
 import com.zz.ejlchina.okhttps.GsonMsgConvertor;
 import com.zz.ejlchina.okhttps.HTTP;
 import com.zz.ejlchina.okhttps.HttpResult;
 import com.zz.region.vo.ResultVO;
+
+import java.io.IOException;
 
 /**
  * @author wqy
@@ -20,6 +23,10 @@ public class HttpTest {
 
         ResultVO resultVO = new ResultVO<String>();
         resultVO = http.sync("").get().getBody().toBean(resultVO.getClass());
+        http.async("").setOnResponse(r->{
+            System.out.println("回调了");
+        }).setOnException((IOException r)->{})
+                .get();
         System.out.println(resultVO);
     }
 
